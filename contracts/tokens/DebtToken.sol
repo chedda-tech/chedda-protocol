d8aa7cb8131b4538ad85b875315ff03d4fdc06ff
John War <john.warmann@gmail.com>
2023-09-27 16:21:23 -0500

    add lending pool contract

--- contracts/tokens/DebtToken.sol
+++ contracts/tokens/DebtToken.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.20;
 import { ERC4626 } from "solmate/mixins/ERC4626.sol";
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { UD60x18, ud } from "prb-math/UD60x18.sol";
-import { console } from "forge-std/console.sol";
 
 contract DebtToken is ERC4626 {
 
