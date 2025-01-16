9d8a4d8711a2a30a32e1b99d8b73193888becdb7
John War <john.warmann@gmail.com>
2025-01-16 11:10:10 -0500

    Fix compiler version in tests

--- test/mocks/MockPriceFeed.sol
+++ test/mocks/MockPriceFeed.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.20;
+pragma solidity 0.8.27;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import { IPriceFeed } from "../../contracts/oracle/IPriceFeed.sol";
